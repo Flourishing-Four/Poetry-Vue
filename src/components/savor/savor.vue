c0116e8cbf6f04b819ff37e5d93f7c08b7a7f570
XYLois <XYLois@outlook.com>
2020-04-23 17:27:34 +0800

    Lois：评论组件修改完成，实现前端评论

--- src/components/savor/savor.vue
+++ src/components/savor/savor.vue
@@ -58,7 +58,7 @@
                 </div>
             </el-card>
             <el-card class="savor-content__card" style="padding: 0;">
-                <comment></comment>
+                <comment @doSend="doSend($event)"  @doChidSend="doChidSend(arguments)" :commentList="commentList" :commentNum="commentNum" :label="label" :avatar="avatar" :placeholder="placeholder" :minRows="minRows" :maxRows="maxRows"></comment>
             </el-card>
         </div>
     </div>
@@ -72,6 +72,133 @@ export default {
   },
   data () {
     return {
+      label: '作者',
+      placeholder: '说点什么吧',
+      minRows: 2,
+      maxRows: 4,
+      commentNum: 2,
+      avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+      commentList: [
+        {
+          id: 999,
+          commentUser: {
+            id: 999,
+            nickName: ' 某知名语文老师',
+            avatar: 'http://qzapp.qlogo.cn/qzapp/101483738/6637A2B6611592A44A7699D14E13F7F7/50'
+          },
+          content: '希望这篇文章能够帮到大家[害羞][害羞][害羞]<br/>',
+          createDate: '2020-4-23 17:36:02',
+          childrenList: [
+            {
+              id: 998,
+              commentUser: {
+                id: 998,
+                nickName: '雾非雾',
+                avatar: require('@/components/img/icon/avtar.png')
+              },
+              targetUser: {
+                id: 999,
+                nickName: '某知名语文老师',
+                avatar: 'http://qzapp.qlogo.cn/qzapp/101483738/6637A2B6611592A44A7699D14E13F7F7/50'
+              },
+              content: '真的就很棒！很Nice!',
+              createDate: '2020-4-23 17:45:26'
+            },
+            {
+              id: 998,
+              commentUser: {
+                id: 998,
+                nickName: '雾非雾',
+                avatar: require('@/components/img/icon/avtar.png')
+              },
+              targetUser: {
+                id: 999,
+                nickName: '某知名语文老师',
+                avatar: 'http://qzapp.qlogo.cn/qzapp/101483738/6637A2B6611592A44A7699D14E13F7F7/50'
+              },
+              content: '真的就很棒！很Nice!',
+              createDate: '2020-4-23 17:45:26'
+            }
+          ]
+        }
+      ],
+      commentUserList: [
+        {
+          id: 999,
+          nickName: '某知名语文老师',
+          avatar: 'http://qzapp.qlogo.cn/qzapp/101483738/6637A2B6611592A44A7699D14E13F7F7/50'
+        },
+        {
+          id: 998,
+          nickName: '雾非雾',
+          avatar: require('@/components/img/icon/avtar.png')
+        },
+        {
+          id: 1,
+          nickName: '林徽因',
+          avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+        }
+      ]
+    }
+  },
+  methods: {
+    // 格式化时间方法
+    getFormatDate () {
+      var date = new Date()
+      var month = date.getMonth() + 1
+      var strDate = date.getDate()
+      if (month >= 1 && month <= 9) {
+        month = '0' + month
+      }
+      if (strDate >= 0 && strDate <= 9) {
+        strDate = '0' + strDate
+      }
+      var currentDate = date.getFullYear() + '-' + month + '-' + strDate +
+            ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+      return currentDate
+    },
+    doSend (newCont) {
+      this.commentList.push({
+        id: '1',
+        commentUser: {
+          id: '1',
+          nickName: '林徽因',
+          avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+        },
+        content: newCont,
+        createDate: this.getFormatDate()
+      })
+      console.log('初始发送按钮点击事件：' + newCont)
+    },
+    doChidSend (args) {
+      const arr = [] // 被评论用户信息
+      for (let i = 0; i < this.commentUserList.length; i++) {
+        if (this.commentUserList[i].id === args[1]) {
+          arr[0] = this.commentUserList[i].id
+          arr[1] = this.commentUserList[i].nickName
+          arr[2] = this.commentUserList[i].avatar
+        }
+      }
+      this.commentList[0].childrenList.push({
+        id: '1',
+        commentUser: {
+          id: '1',
+          nickName: '林徽因',
+          avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+        },
+        targetUser: {
+          id: arr[0],
+          nickName: arr[1],
+          avatar: arr[2]
+        },
+        content: args[0],
+        createDate: this.getFormatDate()
+      })
+      this.commentNum++
+      console.log('评论区发送按钮点击事件：')
+      console.log('评论内容content=' + args[0])
+      console.log('被评论用户targetUserId=' + args[1])
+      console.log('父级评论id=' + args[2])
     }
   }
 }
