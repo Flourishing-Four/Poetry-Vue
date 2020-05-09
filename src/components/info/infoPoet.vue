3589949d990ea43f3ae40dc24eda0dbef53a586d
XYLois <XYLois@outlook.com>
2020-05-09 16:23:25 +0800

    Lois：闻详情页

--- src/components/info/infoPoet.vue
+++ src/components/info/infoPoet.vue
@@ -3,17 +3,18 @@
         <div class="infoPoet-bar">
             <el-card class="infoPoet-bar__card">
                 <div slot="header" class="clearfix">
-                    <span class="poet-name">李白</span>
+                    <span class="poet-name">{{poetList.authorName}}</span>
+                    <span>{{'&nbsp;' + poetList.dynasty}}</span>
                 </div>
                 <div class="item lineHeight">
                     <span class="title">-&nbsp;诗人生平</span>
-                    <div v-for="o in 4" :key="o" class="text">
-                    {{'生平生平生平生平生平 ' + o }}
+                    <div class="text" style="line-height: 28px;">
+                    {{poetList.authorDesc}}
                 </div>
                 </div>
                 <div class="item lineHeight">
                     <span class="title">-&nbsp;诗人轶事</span>
-                    <div v-for="o in 4" :key="o" class="text">
+                    <div v-for="o in 4" :key="o" class="text" style="line-height: 28px;">
                     {{'轶事轶事轶事轶事轶事 ' + o }}
                 </div>
                 </div>
@@ -24,12 +25,12 @@
                 <el-col :span='12'>
                     <el-card class="infoPoet-content__card">
                         <div slot="header" class="clearfix">
-                            <span class="title">将进酒</span>
+                            <span class="title">{{poetWorks[0].poetryTitle}}</span>
                             <el-button style="float: right; padding: 3px 3px" type="text">收藏</el-button>
                             <el-button style="float: right; padding: 3px 3px" type="text">点赞</el-button>
                         </div>
-                        <div class="text item">
-                            <span>你好你好你好</span>
+                        <div class="text item" v-for="(item, index) in poetWorks[0].poetryBody" :key="index">
+                            <span>{{item}}</span>
                         </div>
                     </el-card>
                 </el-col>
@@ -78,6 +79,70 @@
 
 <script>
 export default {
+  data () {
+    return {
+      poetList: this.$route.query.poetList,
+      poetWorks: []
+    }
+  },
+  mounted () {
+    console.log(this.poetList)
+    this.getWorks()
+  },
+  activated () {
+    this.poetList = this.$route.query.poetList
+  },
+  methods: {
+    getWorks () {
+      this.$axios.get('author/authorPoetry', {
+        params: {
+          authorName: this.poetList.authorName
+        }
+      })
+        .then(res => {
+          console.log(res)
+          let list = res.data
+          this.dealWith(list)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    dealWith (val) {
+      // 处理诗文题目
+      for (let i in val) {
+        let str = val[i].poetryTitle.replace(/（[^）]+）/g, '')
+        console.log(str)
+        val[i].poetryTitle = str
+        console.log('val' + val[i].poetryTitle)
+      }
+      // 处理诗文内容
+      for (let m in val) {
+        let str = val[m].poetryBody.replace(/<[^>]+>/g, '') // 去掉htlm标签
+        str = str.replace(/\s/g, '') // 去掉空格
+        str = str.replace(/&nbsp;/ig, '') // 去掉nbsp
+        str = str.replace(/\[.*?\]/g, '') // 去掉[]
+        str = str.replace(/【[^】]+】/g, '') // 去掉【】
+        let list = str.split('。')
+        console.log('0' + list)
+        for (let i in list) {
+          if (list[i] !== '') {
+            list.splice(i, 1, list[i] + '。')
+          }
+          let symbol = ['！', '？', '；']
+          for (let n in symbol) {
+            if (list[i].indexOf(symbol[n]) !== -1) {
+              let str = list[i].replace(/。/g, '') // 将多余的句号删除
+              let arr = str.split(symbol[n])
+              list.splice(i, 1, arr[0] + symbol[n], arr[1])
+            }
+          }
+        }
+        val[m].poetryBody = list
+      }
+      this.poetWorks = val
+    }
+  }
 }
 </script>
 
